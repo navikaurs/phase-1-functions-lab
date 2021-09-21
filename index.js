2181f76d21981dd588cbc3ff87bbad7aa7c9ac55
Navi Kaur <navdeep9520@gmail.com>
2021-09-21 14:19:03 -0700

    commit tree 3a00c2292df8a29b97d2c244c126dd38948f3922

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    if (distance >= 42){
+    return distance - 42
+} 
+else (distance < 42 ) 
+    return 42 - distance
+}
+
+ function distanceFromHqInFeet(distance) {
+    let dist = distanceFromHqInBlocks(distance)
+    console.log (dist * 264)
+    return (dist * 264)
+ }
+ function distanceTravelledInFeet(start , destination) {
+     let blocks
+     if ( start > destination ) 
+        blocks = start - destination
+    if (destination > start)
+     blocks = destination - start
+     return blocks * 264
+ }
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start , destination)
+  let fare 
+    if (feet <= 400){
+        fare = 0
+    }
+    else if ( feet >400 && feet <= 2000){
+        fare = ((feet - 400) * 2) /100 
+    }
+    else if (feet >2000 && feet < 2500) {
+        fare = 25
+    }
+    else {
+        fare = 'cannot travel that far'
+    }
+    return fare
+}
